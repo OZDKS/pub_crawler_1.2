3f57e33eecc3fa2582286344c199f9767994a4d5
Wojciech <pospieszynski.wojciech@gmail.com>
2016-03-14 01:34:55 +0100

    Tried to create emp/own/cust object after registering a user, also found the hole in passing params to sign up form, comment in /register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,26 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:role, :first_name, :last_name, :email, :password, :password_confirmation)
   end
+
+  def after_sign_up
+  	if(User.last.role == "owner")
+  		owner=Owner.create
+  		owner.name="tryyyy"
+  		owner.save
+  		User.last.owner=owner
+  		User.last.save
+  	elsif(User.last.role == "employee")
+  		employee=Employee.create
+  		employee.save
+  		User.last.employee=employee
+  		User.last.save
+
+  	elsif(User.last.role == "customer")
+  		customer=Customer.create
+  		customer.save
+  		User.last.customer=customer
+  		User.last.save
+  	end
+  end
+
 end
