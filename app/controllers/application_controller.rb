24e1d948467abb9f9dcc74eeccaaca263a178b70
Wojciech <pospieszynski.wojciech@gmail.com>
2016-03-22 20:11:10 +0100

    Basic frontend design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
   
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-
-
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
