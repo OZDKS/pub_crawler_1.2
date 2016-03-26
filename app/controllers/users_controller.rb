7a2ea3cb6b3f1ff0308f4eebbdc7789ea84a8428
Wojciech <pospieszynski.wojciech@gmail.com>
2016-03-26 15:20:21 +0100

    Few corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
 # error here?
- before_action :authenticate_user!
+	before_action :authenticate_user!
 
-  def profile
-  	@user = current_user
-  end
+	def profile
+	@user = current_user
+		if @user.user_type_type == "Owner"
+			redirect_to locals_path
+		end
+	end
 end
